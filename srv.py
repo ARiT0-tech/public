6d7d91a2330d218901b6733d96851f0392f012de
Artem Ananev <ananev.artem272005@gmail.com>
2021-03-02 16:06:23 +0300

    n2

--- srv.py
+++ srv.py
@@ -10,5 +10,12 @@ def index():
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    title = 'mars'
+    prof_list = prof.lower()
+    return render_template('base.html', title=title, prof_list=prof_list)
+
+
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
